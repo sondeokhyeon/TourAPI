94124e190fa7ce8889676e7952184b23a5cb404b
손덕현 <sondeokhyeon@gmail.com>
2018-07-21 19:26:10 +0900

    2차 완성

--- js/common.js
+++ js/common.js
@@ -1,7 +1,5 @@
 var common = {
 
-    
-    
     leftMenuImport : function () {
         var $div = $('#fh5co-main-menu');
         $div.load("../cmm/left-menu.html");
@@ -37,10 +35,87 @@ var common = {
                 returnFunction(data)
             }
         });
-    },
+    },   areaChange : function () {
+            $(".areaCodes").click(function() {
+                $(".text-right").css("display","block");
+                draw.areaCode = $(this).attr("data-toggle");
+                $("#travelContents").empty();
+                draw.elementCount = 1;
+                $(".sigunguLists").empty();
+                draw.sigunguCode = '';
+                draw.areaSigunguCodeGet();
+                draw.elements();
+                $(".areaList").css('visibility','hidden');
+                $('.area').text($(this).context.innerHTML);
+                $('.sigungu').text('전체');
+            })
+        },
 
-    getMonth : function() {
-        $('#festivalMonth').append(this.date.getMonth() + 1)
-    },
+        sigunguChange : function () {
+            $(document).on('click', '.sigunguList', function(e) {
+                draw.sigunguCode = $(this).attr("data-toggle");
+                $("#travelContents").empty();
+                draw.elementCount = 1;
+                draw.elements(); 
+                $(".sigunguLists").css('visibility','hidden');
+                $('.sigungu').text($(this).context.innerHTML);
+            })
+        },
 
+        areaDetailCodeParsing : function (data) {
+            var list = data.response.body.items.item
+            var sigunguDraw = '';
+            if (list.code != 1) {
+                $.each(list, function (i, item) {
+                    if (i === 0) {
+                        sigunguDraw +=  "<div class='hiddenMenu'>"
+                                            +"<li class='sigunguList' data-toggle=''>" + '전체' +'</li>' 
+                                            +"<li class='sigunguList' data-toggle="+ item.code +'>'+ item.name +'</li>'
+                    } else if (i % 7 === 0) {
+                        sigunguDraw +=  "</div><div class='hiddenMenu'>"
+                                            +"<li class='sigunguList' data-toggle="+ item.code +'>'+ item.name +'</li>'
+                    } else {
+                        sigunguDraw += "<li class='sigunguList' data-toggle="+ item.code +">"+ item.name +"</li>" 
+                    }
+                })
+                $(".sigunguLists").append(sigunguDraw);
+            } else {
+                sigunguDraw = 
+                    '<span>상세지역이 없습니다</span>'
+                    $(".sigunguLists").append(sigunguDraw)
+            }
+        },	
+
+        buttonAction : function() {
+			$(".area").click(function() {
+				$(".sigunguLists").css('visibility','hidden')
+				$(".areaList").css('visibility','visible')
+			}) 
+
+			$(".sigungu").click(function() {
+				$(".areaList").css('visibility','hidden')
+				$(".sigunguLists").css('visibility','visible')
+			}) 
+
+			$(document).click(function(e) {
+				if(e.target.className == '' ) {
+					if ($(".areaList").css('visibility') != 'hidden') {
+						$(".areaList").css('visibility','hidden')
+					}
+					if ($(".sigunguLists").css('visibility') != 'hidden') {
+						$(".sigunguLists").css('visibility','hidden')
+					}
+				}	
+            });
+        },   
+
+        getMonth : function() {
+            $('#festivalMonth').append(this.date.getMonth() + 1)
+        },
+}
+
+
+function moreView() {
+    draw.elementCount += 1;
+    draw.elements();
 }
