678b55a7deaac15e155821bbb309be4749e31959
sondeokhyeon <sondeokhyeon@gmail.com>
2018-07-24 08:55:14 +0900

    위치정보 일부 기능 추가

--- js/common.js
+++ js/common.js
@@ -120,3 +120,51 @@ function moreView() {
     draw.elementCount += 1;
     draw.elements();
 }
+
+function getLocation() {
+    $('#travelContents').empty();
+    $('#notification').text("위치정보를 가져오고 있습니다...");
+    $('#information').css('display','block');
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getGeolocation, getError, {
+            enableHighAccuracy:true,
+            maximumAge : 0,
+            timeout : 10000
+        }); 
+    } else {
+        alert('현재 사용하는 브라우저는 위치정보를 가져올수 없습니다.')
+    }
+}
+
+//geolocation api 
+//https://hudi.kr/geolocation-api-%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EB%94%94%EB%B0%94%EC%9D%B4%EC%8A%A4-%EC%9C%84%EC%B9%98-%EC%A0%95%EB%B3%B4-%EA%B0%80%EC%A0%B8%EC%98%A4%EA%B8%B0/
+//참고
+
+function getGeolocation(position) {
+    draw.mapY = position.coords.latitude;
+    draw.mapX = position.coords.longitude;
+    $('#moreViewWrap').css('display','block');
+    $('#information').css('display','none');
+    draw.elements();
+}
+
+function getError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("위치정보 확인을 거부하였습니다.")
+            $('#notification').text("위치정보를 확인할 수 없습니다.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("위치정보를 사용할 수 없습니다.")
+            $('#notification').text("위치정보를 확인할 수 없습니다.");
+            break;
+        case error.TIMEOUT:
+            alert("위치정보를 확인하는데 실패하였습니다.\n(TIMEOUT)")
+            $('#notification').text("위치정보를 확인할 수 없습니다.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("위치정보를 확인하는데 실패하였습니다.\n(알수없는 오류)")
+            $('#notification').text("위치정보를 확인할 수 없습니다.");
+            break;
+    }
+}
